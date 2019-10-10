a8dd2a5ea8a49a91d7c5a4862437d9395aef73c0
RichM3 <richmeyerjobs@gmail.com>
2019-10-10 10:38:12 -0400

    corrected missing comma in association model userGroup

--- app/models/userGroup.rb
+++ app/models/userGroup.rb
@@ -1,12 +1,12 @@
 class UserGroup < ApplicationRecord
     validates :user_id, :group_id, pressence: true, uniqueness: true
 
-  has_many :joined_groups
+  has_many :joined_groups,
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :User
     
-  has_many :joined_groups
+  has_many :joined_groups,
     primary_key: :id,
     foreign_key: :group_id,
     class_name: :Group
