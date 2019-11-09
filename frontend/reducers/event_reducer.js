f15ad4c48856f6dd6e09c1dd01458653f744ff55
RichM3 <richmeyerjobs@gmail.com>
2019-11-08 21:25:23 -0500

    created the eventReducer

---
+++ frontend/reducers/event_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_EVENTS } from "../actions/event_actions";
+
+
+const eventsReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    switch (action.type) {
+        case RECEIVE_EVENTS:
+            return action.events;
+        // case RECEIVE_SINGLE_GROUP:
+        //     return Object.assign({}, oldState, { [action.group.id]: action.group })
+        default:
+            return oldState;
+    }
+}
+
+export default eventsReducer;
