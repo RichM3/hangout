e46b104f8427216148ecbd8281d98ac832ee7112
RichM3 <richmeyerjobs@gmail.com>
2019-11-09 23:22:18 -0500

    cleaned out comments in group_create

--- frontend/components/groups/groups_create.jsx
+++ frontend/components/groups/groups_create.jsx
@@ -7,7 +7,6 @@ import FooterContainer from '../footer/footer_container';
 class GroupsCreate extends React.Component {
     constructor(props) {
         super(props);
-        // this.state = this.props;
         this.state = {
             groupname: '',
             description: '',
@@ -37,29 +36,24 @@ class GroupsCreate extends React.Component {
             <NavbarContainer navType={"groups_create"} myType={""} />
             <div className="create-banner">
                 <span className="create-text">Create a Group</span>
-                {/* {this.state.leaderId } */}
             </div>
 
             <div className="group-create-container">
                 <form onSubmit={this.handleSubmit}>
                 <div className="inner-form-container">
                     <div className="inner-item-container" >
-                        {/* <label htmlFor="groupname">Group Name:&nbsp;&nbsp; */}
                         <label htmlFor="groupname">Group Name:</label>
                         <input type="text" autoComplete="off" name="groupname" id="groupname" onChange={this.update('groupname')} />
                     </div>
                     <div className="inner-item-container" >
-                        {/* <label htmlFor="description">Description:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
                         <label htmlFor="description">Description:</label>
                         <textarea type="text" autoComplete="off" name="description" id="description" onChange={this.update('description')} />
                     </div>
                     <div className="inner-item-container">
-                        {/* <label htmlFor="location">Location:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; */}
                         <label htmlFor="location">Location:</label>
                         <input type="text" autoComplete="off" name="location" id="location" onChange={this.update('location')} />
                     </div>
                     <div className="inner-item-container-btn">
-                        {/* <label htmlFor="">test</label>   */}
                         <input className="create-submit" type="submit" value="Submit" />
                     </div>
                 </div>
