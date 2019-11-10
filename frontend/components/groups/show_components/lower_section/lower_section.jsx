0c4e4b31e8c7daa5dae051943c188ddd5b784a2c
RichM3 <richmeyerjobs@gmail.com>
2019-11-09 23:33:36 -0500

    using this.props.type passed through to lowerSection to reflect click

--- frontend/components/groups/show_components/lower_section/lower_section.jsx
+++ frontend/components/groups/show_components/lower_section/lower_section.jsx
@@ -14,12 +14,12 @@ class LowerSection extends React.Component {
     //     });
     // }
 
-    handleUpdate() {
+    // handleUpdate() {
         //by calling this method react re-renders the component
-        this.setState({
-            type: this.props.type
-        });
-    };
+        // this.setState({
+        //     type: this.props.type
+        // });
+    // };
 
     render() {
         // this.setState({
@@ -29,18 +29,18 @@ class LowerSection extends React.Component {
         // handleUpdate;
 
         debugger
-        let words = "test";
+        // let words = "test";
 
-        if (this.state.type === "About") {
-            words = "yes";
-        } else {
-            words = "fail";
-        }
+        // if (this.props.type === "About") {
+        //     words = "yes";
+        // } else {
+        //     words = "fail";
+        // }
 
         return (
             <>
                 <div>LOWER SECTION</div>
-                <div>{words}</div>
+                <div>{this.props.type}</div>
             </>
         )
     }
