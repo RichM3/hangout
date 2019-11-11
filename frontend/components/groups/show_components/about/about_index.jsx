19e0203a82b230e4c85b4701ff724387fb117edf
RichM3 <richmeyerjobs@gmail.com>
2019-11-11 13:43:11 -0500

    added the about shell for groups_show

---
+++ frontend/components/groups/show_components/about/about_index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class AboutIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+
+    render() {
+
+        return (
+            <>
+                <div>ABOUT</div>
+            </>
+        )
+    }
+
+}
+
+export default AboutIndex;
---
+++ frontend/components/groups/show_components/about/about_index_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import AboutIndex from './about_index';
+
+const mapStateToProps = (state, ownProps) => {
+
+    return ({
+    })
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return ({
+    })
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AboutIndex);
