84690410563d322b03b4b98e5ecbf1edc94d629b
RichM3 <richmeyerjobs@gmail.com>
2019-11-12 19:48:29 -0500

    Added some styling to eventItem

--- frontend/components/groups/show_components/events/events_index_item.jsx
+++ frontend/components/groups/show_components/events/events_index_item.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 const EventsIndexItem = props => {
     return (
-        <div className="">
+        <div className="menu-stripe">
+            <div className="menu-div">
             <li>{props.event.eventname}</li>
             <li>{props.event.description}</li>
             <li>{props.event.location}</li>
             {/* <li>{props.event.starttime}</li> */}
+            </div>
         </div>
     )
 };
