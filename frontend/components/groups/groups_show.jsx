13a28239b314bbc765ee8228e0ddb0c7954a7949
RichM3 <richmeyerjobs@gmail.com>
2019-11-19 18:07:26 -0500

    g4

--- frontend/components/groups/groups_show.jsx
+++ frontend/components/groups/groups_show.jsx
@@ -25,8 +25,6 @@ class GroupsShow extends React.Component {
         this.props.deleteGroup(this.state.groupId)
         .then(() => this.props.history.push('/groups'));
     }
-
-
     
     // Need to update state with button???
     leaveGroup(e) {
