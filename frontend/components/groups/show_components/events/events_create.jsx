64d103407283d1374d5bc00b3c6890a884bceebc
RichM3 <richmeyerjobs@gmail.com>
2019-11-24 17:45:39 -0500

    added css code and structure to events_create page

--- frontend/components/groups/show_components/events/events_create.jsx
+++ frontend/components/groups/show_components/events/events_create.jsx
@@ -8,34 +8,57 @@ class EventsCreate extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            eventname: '',
+            description: '',
+            location: '',
+            leaderId: this.props.group.leaderId
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.target.value
+        });
+    }
+
+    // handleSubmit(e) {
+    //     e.preventDefault()
+    //     this.props.createEvent(this.state)
+    //         .then(() => this.props.history.push('/groups'));
+    // }
+
 
     render() {
         return (
         <>
         <NavbarContainer navType={"groups_create"} myType={""} />
-        <div className="create-banner">
-            <span className="create-text">Create an Event</span>
+        <div className="event-create-banner">
+            <span className="event-create-text">Create an Event</span>
         </div>
 
-
-            <div>
-
-                <form action="">
-
-                    <div>Event name</div>
-
-                    <div>Location</div>
-
-                    <div>Description</div>
-
-                </form>
-                
-            </div>
-
-
+        <div className="event-create-container">
+            <form onSubmit={this.handleSubmit}>
+                <div className="event-inner-form-container">
+                    <div className="event-inner-item-container" >
+                        <label htmlFor="groupname">Event Name:</label>
+                        <input type="text" autoComplete="off" name="groupname" id="groupname" onChange={this.update('groupname')} />
+                    </div>
+                    <div className="event-inner-item-container" >
+                        <label htmlFor="description">Description:</label>
+                        <textarea type="text" autoComplete="off" name="description" id="description" onChange={this.update('description')} />
+                    </div>
+                    <div className="event-inner-item-container">
+                        <label htmlFor="location">Location:</label>
+                        <input type="text" autoComplete="off" name="location" id="location" onChange={this.update('location')} />
+                    </div>
+                    <div className="event-inner-item-container-btn">
+                        <input className="event-create-submit" type="submit" value="Submit" />
+                    </div>
+                </div>
+            </form>
+        </div>
 
         <FooterContainer myType={"groups"} />
         </>
