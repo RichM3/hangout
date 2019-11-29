990aa34d43412daddba65f9cc13024382e926be2
RichM3 <richmeyerjobs@gmail.com>
2019-11-28 20:44:01 -0500

    g3

--- frontend/components/groups/show_components/events/events_create.jsx
+++ frontend/components/groups/show_components/events/events_create.jsx
@@ -58,7 +58,6 @@ class EventsCreate extends React.Component {
     }
 
 
-    
     ensureEndGtrStart() {
         if (this.state.starttimevalue >= this.state.endtimevalue) {
             alert("End time is before start time");
