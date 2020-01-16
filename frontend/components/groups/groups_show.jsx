2ebf26c26bcece0869fdf62ffde14e80a6207516
RichM3 <richmeyerjobs@gmail.com>
2020-01-15 19:21:08 -0500

    g2

--- frontend/components/groups/groups_show.jsx
+++ frontend/components/groups/groups_show.jsx
@@ -204,9 +204,6 @@ class GroupsShow extends React.Component {
 
 
 
-
-
-
             <LowerSectionContainer type={this.state.lowerComponent} desc={this.props.group.description} leaderName={this.props.group.leaderName} groupId={this.state.groupId}/>
 
             <FooterContainer myType={"groups"} />
