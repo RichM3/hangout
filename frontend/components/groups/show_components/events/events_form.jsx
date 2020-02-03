da0051381f471cfd9236fe7684df962b5d425c81
RichM3 <richmeyerjobs@gmail.com>
2020-02-02 23:02:54 -0500

    g1

--- frontend/components/groups/show_components/events/events_form.jsx
+++ frontend/components/groups/show_components/events/events_form.jsx
@@ -199,6 +199,15 @@ class EventsCreate extends React.Component {
         }
     }
 
+
+
+
+
+
+
+
+
+    
     setNewLocation() {
         const place = this.locationFinder.getPlace()
         this.setState({
