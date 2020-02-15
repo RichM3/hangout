d0e9511ca2e0bcf8582a953d9d03bb13e7872aa6
RichM3 <richmeyerjobs@gmail.com>
2020-02-14 19:28:10 -0500

    updated to use a default image if the Group does not have a photo

--- app/views/api/groups/index.json.jbuilder
+++ app/views/api/groups/index.json.jbuilder
@@ -3,9 +3,15 @@ json.groups do
         @groups.each do |group|
                 json.set! group.id do
                         json.extract! group, :id, :groupname, :description, :location, :leader_id
-                        #beginning of my changes on 11/6
-                        # json.memberIds group.membership_ids   #pluck(:id)
-                        json.memberIds group.members, :id, :username, :email  #_ids   #s.username  #hips.ids   #pluck(:id)
+                        # debugger
+                        if (group.photo.attached?)
+                                # debugger
+                                json.photoUrl url_for(group.photo)
+                        else
+                                # puts "in else"
+                                json.photoUrl "NA"
+                        end
+                        json.memberIds group.members, :id, :username, :email
                 end
         end
 end
--- frontend/components/groups/groups_index_item.jsx
+++ frontend/components/groups/groups_index_item.jsx
@@ -2,16 +2,60 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const GroupsIndexItem = props => {
-    let fillerImg = window.wafflesURL;
+    // let fillerImg = window.wafflesURL;
+    let fillerImg = window.peopleHangoutURL;
+
+    let bkImage = {
+        backgroundImage: 'url(' + fillerImg + ')'
+    }
+
+    debugger
+    // Can't figure out how to set photoUrl to null yet in JSON
+    if (props.group.photoUrl !== "NA") {
+        // debugger
+        bkImage = {
+            backgroundImage: 'url(' + props.group.photo + ')'
+        }
+    }
 
     return (
-    <div className="fillerImg">
-    <li className="li-formatting">
-    <Link to={`/groups/${props.group.id}`}>{props.group.groupname}</Link>
-    <p>{props.group.location}</p>
-    </li>
-    </div>
+
+        // < div className = { theClass } >
+        <div className="groupItemStyle" style={bkImage}>
+            <li className="li-formatting">
+                <Link to={`/groups/${props.group.id}`}>{props.group.groupname}</Link>
+                <p>{props.group.location}</p>
+            </li>
+        </div>
     )
 };
 
 export default GroupsIndexItem
+
+
+
+
+
+// This is the production code ----------------------------------------------------
+// This is the production code ----------------------------------------------------
+// This is the production code ----------------------------------------------------
+// This is the production code ----------------------------------------------------
+// This is the production code ----------------------------------------------------
+
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// const GroupsIndexItem = props => {
+//     let fillerImg = window.wafflesURL;
+
+//     return (
+//     <div className="fillerImg">
+//     <li className="li-formatting">
+//     <Link to={`/groups/${props.group.id}`}>{props.group.groupname}</Link>
+//     <p>{props.group.location}</p>
+//     </li>
+//     </div>
+//     )
+// };
+
+// export default GroupsIndexItem
