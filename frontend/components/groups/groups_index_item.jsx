e8a291d6a92472aca95544cd2152d0d54c4b6db9
RichM3 <richmeyerjobs@gmail.com>
2020-02-14 19:29:13 -0500

    g3

--- frontend/components/groups/groups_index_item.jsx
+++ frontend/components/groups/groups_index_item.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 const GroupsIndexItem = props => {
     // let fillerImg = window.wafflesURL;
     let fillerImg = window.peopleHangoutURL;
