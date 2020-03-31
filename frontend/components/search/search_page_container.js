bfd8ea917231cb9480da0fcaff3a7b11c644c537
RichM3 <richmeyerjobs@gmail.com>
2020-03-30 23:05:31 -0400

    g3

--- frontend/components/search/search_page_container.js
+++ frontend/components/search/search_page_container.js
@@ -8,8 +8,6 @@ const mapStateToProps = (state, ownProps) => {
     const searchGroupNames = state.entities.searchResults.groupNames ? Object.values(state.entities.searchResults.groupNames) : [];
     const searchEventNames = state.entities.searchResults.eventNames ? Object.values(state.entities.searchResults.eventNames) : [];
 
-
-
     // const searchGroupDescs = state.entities.searchResults.groupDesc ? Object.values(state.entities.searchResults.groupDescs) : [];
     // const searchEventDescs = state.entities.searchResults.groupDesc ? Object.values(state.entities.searchResults.eventDescs) : [];
     // debugger
