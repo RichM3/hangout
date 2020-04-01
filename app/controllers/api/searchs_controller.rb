de96e70350a7ebb0fce98e907f46eec7bbec5a9c
RichM3 <richmeyerjobs@gmail.com>
2020-03-31 22:51:13 -0400

    removed debuggers and comments about no need to extract two arrays of data from search results and combine with JSON.stringify

--- app/controllers/api/searchs_controller.rb
+++ app/controllers/api/searchs_controller.rb
@@ -4,13 +4,14 @@ class Api::SearchsController < ApplicationController
 
         query = get_search_value["searchCriteria"]
 
+        # Code used when extracting data one at a time in anticipation of using JSON.stringify to combine array of objects
         # @groupNames = Group.where('lower(groupname) LIKE ?', "%#{query.downcase}%")
         # @groupDescs = Group.where('lower(description) LIKE ?', "%#{query.downcase}%")
-        # debugger
-        @groupNames = Group.where('lower(groupname) LIKE ? OR lower(description) LIKE ?', "%#{query.downcase}%", "%#{query.downcase}%")
-        # debugger
 
+        @groupNames = Group.where('lower(groupname) LIKE ? OR lower(description) LIKE ?', "%#{query.downcase}%", "%#{query.downcase}%")
         @eventNames = Event.where('lower(eventname) LIKE ? OR lower(description) LIKE ?', "%#{query.downcase}%", "%#{query.downcase}%")
+
+        # Code used when extracting data one at a time in anticipation of using JSON.stringify to combine array of objects
         # @eventNames = Event.where('lower(eventname) LIKE ?', "%#{query.downcase}%")
         # @eventDescs = Event.where('lower(description) LIKE ?', "%#{query.downcase}%")
 
